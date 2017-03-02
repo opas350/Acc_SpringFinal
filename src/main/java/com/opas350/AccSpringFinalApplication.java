96067962748a0fe8bcee53af6fe1953d2e380f7b
opas350 <opas350@gmail.com>
2017-03-02 01:45:48 -0600

    Add first Files

---
+++ src/main/java/com/opas350/AccSpringFinalApplication.java
@@ -0,0 +1,12 @@
+package com.opas350;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccSpringFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccSpringFinalApplication.class, args);
+	}
+}
